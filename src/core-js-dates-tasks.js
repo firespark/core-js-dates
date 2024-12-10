aa27493706142449b0d77eff365303c67f47da5a
fireleit <fireleit@gmail.com>
2024-12-10 15:36:22 +0400

    feat: add solution to function getWorkSchedule

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -293,8 +293,35 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const startDate = new Date(period.start.split('-').reverse().join('-'));
+  const endDate = new Date(period.end.split('-').reverse().join('-'));
+
+  const schedule = [];
+  const currentDate = new Date(startDate);
+
+  const formatedDate = (date) => {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+  };
+
+  let isWorkPeriod = true;
+
+  while (currentDate <= endDate) {
+    if (isWorkPeriod) {
+      for (let i = 0; i < countWorkDays && currentDate <= endDate; i += 1) {
+        schedule.push(formatedDate(currentDate));
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+    } else {
+      currentDate.setDate(currentDate.getDate() + countOffDays);
+    }
+    isWorkPeriod = !isWorkPeriod;
+  }
+
+  return schedule;
 }
 
 /**
